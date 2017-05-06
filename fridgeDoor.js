60867ca4aec4ea621745c60211d9adc4c0f964c1
Tom Dinh <Tom Dinh>
2017-05-05 19:54:49 -0700

    Got template to render and inserting database

--- fridgeDoor.js
+++ fridgeDoor.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
+const DataStore = require('nedb');
+db = new DataStore({filename: 'noteDB', autoload: true});
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view-engine', 'handlebars');
+app.set('view engine', 'handlebars');
 app.use(express.static('public')); // For static assets
 var bodyParser = require('body-parser');
 // create application/x-www-form-urlencoded parser
@@ -15,15 +17,30 @@ var writer = new commonmark.HtmlRenderer();
 // Respond to post request from form page.
 app.post('/notePost', urlencodedParser, function(req, res) {
 	console.log(req.body);
-    res.send(createHtmlMessage(req.body));
+    var content = renderMessage(req.body.user_content);
+    console.log(req.body.user_title);
+    var info = {user_title: req.body.user_title, user_author: req.body.user_author, user_content: content};
+    res.render('note', info);
+    var dateTime = new Date();
+    
+    db.insert(Object.assign({datetime: dateTime.valueOf()},req.body),function(err,newDocs){
+        if(err) {
+            console.log("something went wrong");
+            console.log(err);
+        } else {
+            console.log("added");
+        }
+    });
 });
 
 app.get('/', function(req,res) {
-    res.render('note', req.query);
+    let info = {user_title: "hello", user_author: "yo", datetime: "date", user_content: "# content"};
+   res.render('note', info); 
 });
 
-function createHtmlMessage(info) {
-    let parsed = reader.parse(info.user_content);
+
+function renderMessage(info) {
+    let parsed = reader.parse(info);
     let message = writer.render(parsed);
     return `${message}`;
 }
