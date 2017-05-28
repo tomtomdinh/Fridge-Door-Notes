32abaf26b591be8ff6b2aed8c13aa6d0a42a16de
Tom Dinh <Tom Dinh>
2017-05-27 21:31:10 -0700

    sanitized message

--- fridgeDoor.js
+++ fridgeDoor.js
@@ -1,6 +1,9 @@
+// Tom Dinh Server Side Stuff
+
 var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
+var sanitizeHTML = require('sanitize-html');
 const DataStore = require('nedb');
 db = new DataStore({
     filename: 'noteDB',
@@ -11,9 +14,9 @@ app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
-app.use(express.static('public')); // For static assets
+app.use(express.static('public')); 
 var bodyParser = require('body-parser');
-// create application/x-www-form-urlencoded parser
+
 var urlencodedParser = bodyParser.urlencoded({
     extended: false
 })
@@ -21,62 +24,68 @@ var commonmark = require('commonmark');
 var reader = new commonmark.Parser();
 var writer = new commonmark.HtmlRenderer();
 
+// goes to the default page where it renders all the notes
 app.get('/', function (req, res) {
-    db.find({}).sort({"datetime": 1}).exec(function (err, docs) {
+    db.find({}).sort({"datetime": -1}).exec(function (err, docs) {
         if (err) {
             console.log("Something went wrong");
         } else {
             console.log("it worked");
             for(var d of docs) {
-                var date = new Date(d.datetime);
-                d.datetime = date.toLocaleString();
+                d.datetime = new Date(d.datetime).toLocaleString();
             }
             res.render('allNotes',{notes: docs});
         }
     });
 });
-
+// responds to post deleteform, deletes all the checked boxes
 app.post('/deleteForm', urlencodedParser, function(req,res) {
     console.log(req.body);
     var id = Object.keys(req.body);
-    for(var i = 0 ; i < id.length; i++) {
-    db.remove({_id : id[i] }, {multi:true}, function(err,numRemoved) {
+    var toDel = [];
+    for(var i of id) {
+        toDel.push({_id: i});
+    }
+    console.log(toDel);
+//    for(var i = 0 ; i < id.length; i++) {
+    db.remove({$or: toDel}, {multi:true}, function(err,numRemoved) {
         if(err) {
             console.log("something went wrong");
         } else {
-            console.log("numremoved " + numRemoved);
+            console.log("removed " + numRemoved);
         }
-    })
-    }
+    });
+//    }
     res.redirect('/');
 });
-
+// gets the deleteform after clicking the link
 app.get('/deleteForm', function(req,res){
-        console.log("delete form");
-      db.find({}).sort({"datetime": 1}).exec(function (err, docs) {
+      
+      db.find({}).sort({"datetime": -1}).exec(function (err, docs) {
         if (err) {
             console.log("Something went wrong");
         } else {
-            console.log("it worked");
+             console.log("delete form");
             for(var d of docs) {
-                var date = new Date(d.datetime);
-                d.datetime = date.toLocaleString();
+                d.datetime = new Date(d.datetime).toLocaleString();
             }
             res.render('deleteForm',{notes: docs});
         }
     });
    
 });
-
+// just gets the noteform
 app.get('/noteForm', function(req,res) {
    res.render('noteForm') ;
 });
 
-// Respond to post request from form page.
+// Respond to post request from new note page
 app.post('/notePost', urlencodedParser, function (req, res) {
     var dateTime = new Date();
-    req.body.user_content = renderMessage(req.body.user_content);
-    db.insert(Object.assign({datetime: dateTime.valueOf()}, req.body), function (err, newDocs) {
+    var myObj = req.body;
+    myObj.user_content = renderMessage(myObj.user_content);
+    
+    db.insert(Object.assign({datetime: dateTime.valueOf()}, myObj), function (err, newDocs) {
         if (err) {
             console.log("something went wrong");
             console.log(err);
@@ -86,10 +95,9 @@ app.post('/notePost', urlencodedParser, function (req, res) {
         }
     });
 });
-
+// Respond to get after clicking note link
 app.get('/note/:noteID', function(req,res) {
     var id = req.params.noteID;
-   // console.log(id);
     db.findOne({_id: id}, function(err,docs) {
         if(err) {
             console.log("Something went wrong");
@@ -100,10 +108,12 @@ app.get('/note/:noteID', function(req,res) {
         }
     });
 });
-
+// renders the message so that it works with markdown
 function renderMessage(info) {
-    let parsed = reader.parse(info);
+    let m = sanitizeHTML(info);
+    let parsed = reader.parse(m);
     let message = writer.render(parsed);
+    
     return `${message}`;
 }
 
